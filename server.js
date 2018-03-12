4d7cb1d4ba31e71471c07d5931b278ffc8f62089
shammelow <admin@steffenhammelowberg.com>
2018-03-12 11:12:16 +0700

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname +'/public'));
@@ -45,8 +47,8 @@ app.get('/bad',(req, res) => {
         errorMessage: 'Unable to handle request.'
     });
 });
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
 
 
